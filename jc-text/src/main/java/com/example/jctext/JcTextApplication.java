44f9421f7e3d20f3198cf57682abccdc1b1e25ac
Jayce <sunjie_peace@163.com>
2018-12-10 13:38:27 +0800

    test新增

---
+++ jc-text/src/main/java/com/example/jctext/JcTextApplication.java
@@ -0,0 +1,12 @@
+package com.example.jctext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcTextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcTextApplication.class, args);
+    }
+}
---
+++ jc-text/src/test/java/com/example/jctext/JcTextApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jctext;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JcTextApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
