04ac219140b96bcc8464a4120ae14267e026db6f
Jayce <sunjie_peace@163.com>
2018-12-20 14:47:48 +0800

    eureka 注册中心

--- jc-cloud/jc-text/src/main/java/com/example/jctest/JcTextApplication.java
+++ jc-cloud/jc-text/src/main/java/com/example/jctest/JcTextApplication.java
@@ -2,10 +2,11 @@ package com.example.jctest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class JcTextApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JcTextApplication.class, args);
     }
