494d570a86eb4a5c8d4d59dcd10f91c630a64a46
Jayce <sunjie_peace@163.com>
2019-01-20 17:02:47 +0800

    feign-api

---
+++ me-exercise/src/main/java/com/exercise/model/example/TempTest.java
@@ -0,0 +1,13 @@
+package com.exercise.model.example;
+
+public class TempTest {
+    public static void main(String[] args) {
+        Integer n = null;
+        Integer m = 1;
+        if (n != null && n.equals(m)) {
+            System.out.println("hahahahah");
+        } else {
+            System.out.println("646464646");
+        }
+    }
+}
