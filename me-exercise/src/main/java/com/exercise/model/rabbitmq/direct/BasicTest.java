9822bbe8fefe0c13124310df203a8cc82947dbf4
jayce <sunjie_peace@163.com>
2021-05-19 20:49:15 +0800

    Update BasicTest.java
    aa

--- me-exercise/src/main/java/com/exercise/model/rabbitmq/direct/BasicTest.java
+++ me-exercise/src/main/java/com/exercise/model/rabbitmq/direct/BasicTest.java
@@ -10,11 +10,11 @@ import java.util.concurrent.Executors;
 public class BasicTest extends Application {
 
     // rabbitmq的IP地址
-    private final String rabbitmq_host = "10.35.106.153";
+    private final String rabbitmq_host = "10.xx.xx.xx";
     // rabbitmq的用户名称
-    private final String rabbitmq_user = "root";
+    private final String rabbitmq_user = "xx";
     // rabbitmq的用户密码
-    private final String rabbitmq_pwd = "dahuacloud";
+    private final String rabbitmq_pwd = "xx";
     // 测试线程池
     private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
